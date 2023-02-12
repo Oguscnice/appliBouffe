3eaa0e4c304ce5ce8e22fc91cfa042f3daf538e1
Oguscnice <guillaume_cometto@hotmail.fr>
2023-02-12 11:00:10 +0100

    good advanced

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./dropdown.component.scss']
 })
 export class DropdownComponent {
+  dropdownHave : boolean = false;
+  dropdownHavent : boolean = false;
+  ingredientHave : string[]=[]
+  ingredientHavent : string[]=[]
 
+  iHaveVissibility(){
+    this.dropdownHave = !this.dropdownHave
+  }
+
+  iHaventVissibility(){
+    this.dropdownHavent = !this.dropdownHavent
+  }
+
+  updateDropdownHave(ingredientCheckboxed:string):void {
+    if (this.ingredientHave.includes(ingredientCheckboxed)) {
+      this.ingredientHave = this.ingredientHave.filter(
+        (genre) => genre !== ingredientCheckboxed
+      );
+    } else {
+      this.ingredientHave.push(ingredientCheckboxed);
+    }
+  }
+
+  updateDropdownHavent(ingredientCheckboxed:any):void{
+    if (this.ingredientHavent.includes(ingredientCheckboxed)) {
+      this.ingredientHavent = this.ingredientHavent.filter(
+        (genre) => genre !== ingredientCheckboxed
+      );
+    } else {
+      this.ingredientHavent.push(ingredientCheckboxed);
+    }
+  }
 }
