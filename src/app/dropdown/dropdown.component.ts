720a1bcf6c979e9b0020c062278800c21d33f2f2
Oguscnice <guillaume_cometto@hotmail.fr>
2023-02-13 10:50:22 +0100

    3/4 ingredientsList traduct

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -38,8 +38,7 @@ export class DropdownComponent {
       this.ingredientHave.push(ingredientCheckboxed);
     }
     this.ingredientArrayHaveFromDropdown.emit(this.ingredientHave);
-    console.log("Have"+this.ingredientHave);
-  }
+ }
 
   updateDropdownHavent(ingredientCheckboxed: any): void {
     if (this.ingredientHavent.includes(ingredientCheckboxed)) {
@@ -50,8 +49,6 @@ export class DropdownComponent {
       this.ingredientHavent.push(ingredientCheckboxed);
     }
     this.ingredientArrayHaventFromDropdown.emit(this.ingredientHavent);
-    console.log("Havent"+this.ingredientHavent);
-
   }
 
 filterNameWithInputTextFromHeader(ingredientNameFromDataTS : string):boolean {
